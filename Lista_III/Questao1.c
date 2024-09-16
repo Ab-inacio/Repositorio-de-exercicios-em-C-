df2c4a25aa3a4b59b1c2d03c89c7d54f5474e072
Ab-inacio~ <inacioantoniobastos0@gmail.com>
2024-09-16 12:49:46 -0300

    adicionando lista III

---
+++ Lista_III/Questao1.c
@@ -0,0 +1,37 @@
+#include <stdio.h> 
+
+int main () {
+    
+    int n, i, j;
+    char temp;
+    
+    printf ("Digite a quantidade de letras que deseja inserir: ");
+    scanf ("%d", &n);
+    
+    char letras[n];
+    
+    printf ("Digite as letras:\n");
+    for (i = 0; i < n; i++) {
+        scanf (" %c", &letras[i]);
+    }
+    
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+        if (letras[j] > letras[j+1]) {
+           
+           temp = letras[j];
+           letras[j] = letras[j+1];
+           letras[j+1] = temp;
+        }
+        }
+    }
+    
+    printf ("Letras ordenadas: ");
+    for (i = 0; i < n; i++) {
+        printf ("%c ", letras[i]);
+    }
+    
+    printf ("\n");
+    
+    return 0; 
+}
